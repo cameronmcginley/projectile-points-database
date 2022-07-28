728a6196e8d1aeebd935666db018483f3d8ac550
Cameron McGinley <mcginleycameron@gmail.com>
2022-07-27 22:19:51 -0400

    Add header and data to points page

--- pages/points/[name].tsx
+++ pages/points/[name].tsx
@@ -12,9 +12,9 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 import ReactMarkdown from "react-markdown";
-import testmd from "./test.md";
 
 import React from "react";
 import { dehydrate, useQuery } from "react-query";
@@ -41,6 +41,12 @@ const Points: NextPageWithLayout = ({ name }) => {
   const { data } = useQuery("point", () => getPointByName({ name }));
   console.log(data);
   console.log("test");
+
+  const header_data = [
+    ["Variant", "test"],
+    ["AKA", "test"],
+    ["Short Fot", "test"],
+  ];
   return (
     <>
       {/* <div>{JSON.stringify(data)}</div> */}
@@ -49,76 +55,134 @@ const Points: NextPageWithLayout = ({ name }) => {
         <p>test</p>
       </Paper> */}
       {data && (
-        <Grid container columnSpacing={2}>
-          <Grid xs item style={{ minWidth: "16rem" }}>
-            <Paper sx={{ p: 1 }}>
-              {/* <h1>{data.point.name}</h1>
+        <>
+          <Paper
+            sx={{
+              p: 1,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              mb: 2,
+            }}
+          >
+            <Typography variant="h3">{data.point.name}</Typography>
+            {/* <Typography variant="subtitle1">Variant: {"test"}</Typography>
+            <Typography variant="subtitle1">AKA: {"test"}</Typography>
+            <Typography variant="subtitle1">Short For: {"test"}</Typography> */}
+            <TableContainer sx={{ p: 1, width: "auto" }}>
+              <Table
+                size="small"
+                aria-label="simple table"
+                style={{ width: "auto" }}
+              >
+                <TableBody>
+                  {header_data.map((item) => {
+                    return (
+                      <TableRow key={item[0]} sx={{ "& td": { border: 0 } }}>
+                        <TableCell sx={{ pb: 0 }} align="right">
+                          {item[0] + ":"}
+                        </TableCell>
+                        <TableCell sx={{ pl: 0, pb: 0 }}>{item[1]}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                  {/* <TableRow sx={{ "& td": { border: 0 } }}>
+                    <TableCell sx={{ pb: 0 }} align="right">
+                      {"Variant: "}
+                    </TableCell>
+                    <TableCell sx={{ pl: 0, pb: 0 }}>{"test"}</TableCell>
+                  </TableRow>
+                  <TableRow sx={{ "& td": { border: 0 } }}>
+                    <TableCell sx={{ pb: 0 }} align="right">
+                      {"AKA: "}
+                    </TableCell>
+                    <TableCell sx={{ pl: 0, pb: 0 }}>{"test"}</TableCell>
+                  </TableRow>
+                  <TableRow sx={{ "& td": { border: 0 } }}>
+                    <TableCell sx={{ pb: 0 }} align="right">
+                      {"Short For: "}
+                    </TableCell>
+                    <TableCell sx={{ pl: 0, pb: 0 }}>{"test"}</TableCell>
+                  </TableRow> */}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+          <Grid container columnSpacing={2}>
+            <Grid xs item style={{ minWidth: "16rem" }}>
+              <Paper sx={{ p: 1 }}>
+                {/* <h1>{data.point.name}</h1>
               <div>{JSON.stringify(data)}</div> */}
-              <ReactMarkdown>{data.point.description}</ReactMarkdown>
-            </Paper>
-          </Grid>
-          <Grid item style={{ minWidth: "24rem" }}>
-            {/* Main details */}
-            <Paper>
-              <TableContainer sx={{ p: 1 }}>
-                <Table size="small" aria-label="simple table">
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>{"Date"}</TableCell>
-                      <TableCell>{data.point.date_info}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>{"Cultural Period"}</TableCell>
-                      <TableCell>{data.point.cultural_period}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>{"Glacial Period"}</TableCell>
-                      <TableCell>{data.point.glacial_period}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>{"Culture"}</TableCell>
-                      <TableCell>{data.point.culture}</TableCell>
-                    </TableRow>
-                    <TableRow
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell>{"Tradition"}</TableCell>
-                      <TableCell>{data.point.tradition}</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </Paper>
+                <ReactMarkdown>{data.point.description}</ReactMarkdown>
+              </Paper>
+            </Grid>
+            <Grid item style={{ minWidth: "24rem" }}>
+              {/* Main details */}
+              <Paper>
+                <TableContainer sx={{ p: 1 }}>
+                  <Table size="small" aria-label="simple table">
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>{"Date"}</TableCell>
+                        <TableCell>{data.point.date_info}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>{"Cultural Period"}</TableCell>
+                        <TableCell>{data.point.cultural_period}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>{"Glacial Period"}</TableCell>
+                        <TableCell>{data.point.glacial_period}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>{"Culture"}</TableCell>
+                        <TableCell>{data.point.culture}</TableCell>
+                      </TableRow>
+                      <TableRow
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell>{"Tradition"}</TableCell>
+                        <TableCell>{data.point.tradition}</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Paper>
 
-            {/* Name details */}
-            <Paper sx={{ mt: 2 }}>
-              <TableContainer sx={{ p: 1 }}>
-                <Table size="small" aria-label="simple table">
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>{"Named By"}</TableCell>
-                      <TableCell>{data.point.date_info}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>{"Named For"}</TableCell>
-                      <TableCell>{data.point.cultural_period}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>{"Year Identified"}</TableCell>
-                      <TableCell>{data.point.glacial_period}</TableCell>
-                    </TableRow>
-                    <TableRow
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell>{"Type Site"}</TableCell>
-                      <TableCell>{data.point.tradition}</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </Paper>
+              {/* Name details */}
+              <Paper sx={{ mt: 2 }}>
+                <TableContainer sx={{ p: 1 }}>
+                  <Table size="small" aria-label="simple table">
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>{"Named By"}</TableCell>
+                        <TableCell>{data.point.date_info}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>{"Named For"}</TableCell>
+                        <TableCell>{data.point.cultural_period}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>{"Year Identified"}</TableCell>
+                        <TableCell>{data.point.glacial_period}</TableCell>
+                      </TableRow>
+                      <TableRow
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell>{"Type Site"}</TableCell>
+                        <TableCell>{data.point.tradition}</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Paper>
+            </Grid>
           </Grid>
-        </Grid>
+        </>
       )}
     </>
   );
