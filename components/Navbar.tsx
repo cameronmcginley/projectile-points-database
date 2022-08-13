fda3b92c303b5942edbd1a7e469d2817f59dda01
Cameron McGinley <mcginleycameron@gmail.com>
2022-08-12 21:05:42 -0400

    Restyled navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,58 +1,56 @@
 import * as React from "react";
-import { AppBar, Box, Typography, Container } from "@mui/material";
+import { AppBar, Box, Typography, Container, Paper } from "@mui/material";
 import theme from "./palette";
 
 const Navbar = () => {
   return (
     <>
-      <AppBar
-        position="static"
-        sx={{
-          backgroundColor: "white",
-          height: "5vh",
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Container maxWidth="xl">
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            {/* Title */}
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                fontWeight: 700,
-                color: theme.palette.text,
-                textDecoration: "none",
-                "&:hover": {
-                  color: theme.palette.primary.main,
-                },
-              }}
-            >
-              Projectile Points Revamped
-            </Typography>
-          </Box>
-        </Container>
-      </AppBar>
-      <AppBar
-        position="static"
-        sx={{
-          backgroundColor: theme.palette.primary.main,
-          height: "1vh",
-        }}
-      />
+      {/* Main title */}
+      <Box sx={{
+        width: "100%", height: "5vh", backgroundColor: "white", display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        // boxShadow: 2,
+        zIndex: 1,
+        position: "relative"
+      }}>
+        {/* Title */}
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            fontWeight: 700,
+            color: theme.palette.text,
+            textDecoration: "none",
+            "&:hover": {
+              color: theme.palette.primary.main,
+            },
+          }}
+        >
+          Projectile Points Revamped
+        </Typography>
+      </Box>
+
+      {/* Links */}
+      <Box className="inset-shadow" sx={{
+        width: "100%",
+        height: "5vh",
+        backgroundColor: "white",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "relative",
+      }}>
+        Title
+      </Box>
+
+      <Box className="bottom-shadow" sx={{ width: "100%", height: ".8vh", backgroundColor: theme.palette.primary.main }} />
     </>
+
   );
 };
 export default Navbar;
