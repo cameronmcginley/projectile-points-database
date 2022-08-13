bfc6a6f9c05d773514120669fd97df0acefa8b35
Cameron McGinley <mcginleycameron@gmail.com>
2022-08-12 21:34:46 -0400

    Added nav links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,39 @@
 import * as React from "react";
-import { AppBar, Box, Typography, Container, Paper } from "@mui/material";
+import { AppBar, Box, Typography, Container, Button } from "@mui/material";
 import theme from "./palette";
+import NavbarLink from "./NavbarLink";
 
 const Navbar = () => {
   return (
     <>
       {/* Main title */}
-      <Box sx={{
-        width: "100%", height: "5vh", backgroundColor: "white", display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        // boxShadow: 2,
-        zIndex: 1,
-        position: "relative"
-      }}>
+      <Box
+        sx={{
+          width: "100%",
+          height: "5vh",
+          backgroundColor: "white",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          // boxShadow: 2,
+          zIndex: 1,
+          position: "relative",
+        }}
+      >
         {/* Title */}
         <Typography
           variant="h6"
           noWrap
           component="a"
-          href="/"
+          // href="/"
           sx={{
             fontWeight: 700,
             color: theme.palette.text,
             textDecoration: "none",
-            "&:hover": {
-              color: theme.palette.primary.main,
-            },
+            // "&:hover": {
+            //   color: theme.palette.primary.main,
+            // },
           }}
         >
           Projectile Points Revamped
@@ -35,21 +41,26 @@ const Navbar = () => {
       </Box>
 
       {/* Links */}
-      <Box className="navbar-border" sx={{
-        width: "100%",
-        height: "5vh",
-        backgroundColor: "white",
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-      }}>
-        Title
+      <Box
+        className="navbar-border"
+        sx={{
+          width: "100%",
+          height: "5vh",
+          backgroundColor: "white",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <NavbarLink text="Search" link="/points" />
+        <NavbarLink text="Upload" link="/points/upload" />
+        <NavbarLink text="Home" link="/" />
+        <NavbarLink text="Contact" link="/contact" />
       </Box>
 
       {/* <Box className="bottom-shadow" sx={{ width: "100%", height: ".8vh", backgroundColor: theme.palette.primary.main }} /> */}
     </>
-
   );
 };
 export default Navbar;
---
+++ components/NavbarLink.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { AppBar, Box, Typography, Container, Button } from "@mui/material";
+import theme from "./palette";
+
+const NavbarLink = (props) => {
+  return (
+    <>
+      <Button
+        sx={{
+          height: "100%",
+          border: 0,
+          //   borderRight: 1,
+          //   borderLeft: 1,
+          borderRadius: 0,
+          p: 2,
+          "&:hover": {
+            borderTop: 0,
+            borderBottom: 0,
+          },
+        }}
+        href={props.link}
+        variant="outlined"
+      >
+        {props.text}
+      </Button>
+    </>
+  );
+};
+export default NavbarLink;
